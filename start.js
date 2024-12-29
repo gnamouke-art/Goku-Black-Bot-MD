1ff4e04a1c814f6c659dce33ee9602d08249f514
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2024-12-29 11:19:32 -0300

    Actualizar start.js

--- start.js
+++ start.js
@@ -22,7 +22,7 @@ import {makeWASocket, protoType, serialize} from './lib/simple.js'
 import {Low, JSONFile} from 'lowdb'
 import {mongoDB, mongoDBV2} from './lib/mongoDB.js'
 import store from './lib/store.js'
-import { BlackJadiBot } from './plugins/jadibot-serbot.js';
+//import { BlackJadiBot } from './plugins/jadibot-serbot.js';
 const {proto} = (await import('@whiskeysockets/baileys')).default
 const {DisconnectReason, useMultiFileAuthState, MessageRetryMap, fetchLatestBaileysVersion, makeCacheableSignalKeyStore, jidNormalizedUser, PHONENUMBER_MCC} = await import('@whiskeysockets/baileys')
 import readline from 'readline'
