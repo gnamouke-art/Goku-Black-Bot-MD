e9d6ade2cc7c2d64409bae290eb7cfdfec35a6ea
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-01-29 12:13:44 -0300

    ʜᴏʟᴀᴀᴀ

--- plugins/ia.gemini.js
+++ plugins/ia.gemini.js
@@ -1,19 +1,19 @@
-//Creditos a Dev-Diego 
 import fetch from 'node-fetch'
+
 var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `『🫧』𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙪𝙣 𝙩𝙚𝙭𝙩𝙤 𝙥𝙖𝙧𝙖 𝙪𝙨𝙖𝙧 𝙚𝙨𝙩𝙚 𝙘𝙤𝙢𝙖𝙣𝙙𝙤.\n\n• 𝙋𝙤𝙧 𝙚𝙟𝙚𝙢𝙥𝙡𝙤:\n${usedPrefix + command} Hola`, m, rcanal)
+if (!text) return conn.reply(m.chat, `✨ *Ingresé una petición*\n\nEjemplo, ${usedPrefix + command} hola, que es HuTao?`, m)
 try {
-await m.react(rwait)
+await m.react('🕒')
 conn.sendPresenceUpdate('composing', m.chat)
 var apii = await fetch(`https://apis-starlights-team.koyeb.app/starlight/gemini?text=${text}`)
 var res = await apii.json()
-await m.reply(res.result)
-} catch {
-await m.react(error)
-await conn.reply(m.chat, `『❌』𝙊𝙘𝙪𝙧𝙧𝙞𝙤 𝙪𝙣 𝙚𝙧𝙧𝙤𝙧 𝙚𝙣 𝙚𝙡 𝙘𝙤𝙢𝙖𝙣𝙙𝙤, 𝙧𝙚𝙥𝙤𝙧𝙩𝙖𝙡𝙤 𝙖𝙡 𝙘𝙧𝙚𝙖𝙙𝙤𝙧 𝙙𝙚𝙡 𝙗𝙤𝙩.`, m, rcanal)
+await conn.reply(m.chat, res.result, m)
+await m.react('✅️')
+} catch (error) {
+return conn.reply(m.chat, 'Enseñame que responder.', m)
 }}
+
 handler.command = ['gemini']
 handler.help = ['gemini']
 handler.tags = ['ai']
-
 export default handler
