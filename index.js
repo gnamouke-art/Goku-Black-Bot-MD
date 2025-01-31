1c4809074c8780f95b48817920b1faab2736b419
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-01-31 14:02:03 -0300

    Actualizar index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando HuTao ✰');
+console.log('\n✰ Iniciando GokuBlack ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('HuTao\nProyect', {
+say('Goku-Black-Bot\nMD', {
 font: 'block',
 align: 'center',
 colors: ['red']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • Xi_Miguelon`, {
+say(`Developed By • Ivan`, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
