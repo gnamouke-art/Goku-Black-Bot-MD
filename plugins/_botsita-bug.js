7b1b7c299302976be5d1dd75a8d9d7d444028812
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-02 19:36:05 -0300

    CAMI-IA

--- plugins/_botsita-bug.js
+++ plugins/_botsita-bug.js
@@ -1,12 +1,12 @@
 import fetch from 'node-fetch'
 
 let HS = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, '❀ ingresa un texto para hablar con chatgpt', m)
+if (!text) return conn.reply(m.chat, '`ingresa un texto para hablar con Cami-ia`', m)
 //si borras creditos eri gei 👀
 try {
 let api = await fetch(`https://api.davidcyriltech.my.id/ai/chatbot?query=${text}`)
 let json = await api.json()
-let JT = { contextInfo: { externalAdReply: { title: 'Chatgpt - Ai', body: null,  mediaType: 1, previewType: 0, mediaUrl: 'https://chatgpt.com', sourceUrl: 'https://chatgpt.com', thumbnailUrl: 'https://i.pinimg.com/originals/9d/e4/97/9de497cbac67554d199b5945006f14d0.jpg', renderLargerThumbnail: false }}}
+let JT = { contextInfo: { externalAdReply: { title: 'Cami - Ai', body: null,  mediaType: 1, previewType: 0, mediaUrl: 'https://chatgpt.com', sourceUrl: 'https://chatgpt.com', thumbnailUrl: 'https://qu.ax/OfoAC.jpg', renderLargerThumbnail: false }}}
 
   await conn.reply(m.chat, json.result, m, JT)
 
