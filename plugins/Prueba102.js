d678535086971458b793b996bd2bd4e93d09a4a5
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-02 18:00:08 -0300

    xk

--- plugins/Prueba102.js
+++ plugins/Prueba102.js
@@ -4,10 +4,10 @@
 import axios from 'axios'
 
 let HS = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, `oye pendejo deberías de poner un link de YouTube :D`, m)
+if (!text) return conn.reply(m.chat, `❀ Ingresa un link de YouTube`, m)
   
 try {
-let api = await axios.get(`https://mahiru-shiina.vercel.app/download/ytmp4?url=${text}`)
+let api = await axios.get(`https://mahiru-shiina.vercel.app/download/ytmp3?url=${text}`)
 let json = api.data
 
 let { title, description, uploaded, duration, views, type, url, thumbnail, author, download } = json.data
@@ -22,12 +22,12 @@ let HS = `- *Titulo:* ${title}
 - *Vistas:* ${views}`
 
 await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: HS }, { quoted: m })
-await conn.sendMessage(m.chat, { video: { url: download }, mimetype: 'video/mp4'  }, { quoted: m })
+await conn.sendMessage(m.chat, { audio: { url: download }, mimetype: 'audio/mpeg' }, { quoted: m })
     
 } catch (error) {
 console.error(error)
 }}
 
-HS.command = ['crx']
+HS.command = ['ytmp3']
 
 export default HS
