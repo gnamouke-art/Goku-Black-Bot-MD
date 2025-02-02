900f890c79368028e1d132751a726070b99e189c
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-02 19:43:27 -0300

    Actualizar _botsita-bug.js

--- plugins/_botsita-bug.js
+++ plugins/_botsita-bug.js
@@ -5,7 +5,7 @@ if (!text) return conn.reply(m.chat, '`ingresa un texto para hablar con Cami-ia`
 try {
 let api = await fetch(`https://api.davidcyriltech.my.id/ai/chatbot?query=${text}`)
 let json = await api.json()
-let JT = { contextInfo: { externalAdReply: { title: 'Cami - Ai', body: null,  mediaType: 1, previewType: 0, mediaUrl: 'https://Cami-IA.com', sourceUrl: 'https://Cami-IA.com', thumbnailUrl: 'https://qu.ax/OfoAC.jpg', renderLargerThumbnail: false }}}
+let JT = { contextInfo: { externalAdReply: { title: 'Cami - IA', body: null,  mediaType: 1, previewType: 0, mediaUrl: 'https://Cami-IA.com', sourceUrl: 'https://Cami-IA.com', thumbnailUrl: 'https://qu.ax/OfoAC.jpg', renderLargerThumbnail: false }}}
 
   await conn.reply(m.chat, json.result, m, JT)
 
