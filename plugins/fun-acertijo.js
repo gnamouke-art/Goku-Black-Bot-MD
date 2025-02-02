d8a72df696f0cd51fe787636a3dc9ac7bcca5fed
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-02 20:18:44 -0300

    Actualizar fun-acertijo.js

--- plugins/fun-acertijo.js
+++ plugins/fun-acertijo.js
@@ -8,7 +8,7 @@ const handler = async (m, {conn, usedPrefix}) => {
     conn.reply(m.chat, 'Todavía hay acertijos sin responder en este chat', conn.tekateki[id][0]);
     throw false;
   }
-  const tekateki = JSON.parse(fs.readFileSync(`./src/database/acertijo.js`));
+  const tekateki = JSON.parse(fs.readFileSync(`./src/game/acertijo.json`));
   const json = tekateki[Math.floor(Math.random() * tekateki.length)];
   const _clue = json.response;
   const clue = _clue.replace(/[A-Za-z]/g, '_');
@@ -28,5 +28,6 @@ const handler = async (m, {conn, usedPrefix}) => {
 };
 handler.help = ['acertijo'];
 handler.tags = ['fun'];
-handler.command = ['acertijo', 'acert', 'adivinanza', 'tekateki'];
+handler.group = true;
+handler.command = ['acertijo','acert','adivinanza','tekateki']
 export default handler;
