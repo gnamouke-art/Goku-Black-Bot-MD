07430003895973bb2e2766f3262be493c58badc1
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-02 18:43:02 -0300

    Goku-Black-Bot-MD

--- plugins/Prueba102.js
+++ plugins/Prueba102.js
@@ -1,28 +1,3 @@
 /*
 - GRACIAS POR VER ESTE ARCHIVO 
 */
-//By JTxs  Ivanmods15
-// *[ ❀ PLAY ]*
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `Ingresa el nombre de la cancion que quieras buscar`, m)
-
-try {
-let api = await fetch(`https://api.vreden.web.id/api/ytplaymp3?query=${text}`)
-let json = await api.json()
-let { title, thumbnail, timestamp, ago, views, author } = json.result.metadata
-let HS = `- *Titulo :* ${title}
-- *Duracion :* ${timestamp}
-- *Subido :* ${ago}
-- *Visitas :* ${views}
-- *Autor :* ${author.name}`
-await conn.sendFile(m.chat, thumbnail, 'HasumiBotFreeCodes.jpg', HS, m)
-await conn.sendFile(m.chat, json.result.download.url, 'HasumiBotFreeCodes.mp3', null, m)
-} catch (error) {
-console.error(error)
-}}
-
-handler.command = /^(kadu)$/i
-
-export default handler
