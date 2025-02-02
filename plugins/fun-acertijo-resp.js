bb474425ddb1ab926e8d31a6730308b43b4bd096
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-02 20:14:34 -0300

    Actualizar fun-acertijo-resp.js

--- plugins/fun-acertijo-resp.js
+++ plugins/fun-acertijo-resp.js
@@ -1,22 +1,27 @@
-import similarity from 'similarity';
-const threshold = 0.72;
-const handler = (m) => m;
-handler.before = async function(m) {
-  const id = m.chat;
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
-  this.tekateki = this.tekateki ? this.tekateki : {};
-  if (!(id in this.tekateki)) return m.reply('✨️ Ese acertijo ya ha terminado!');
-  if (m.quoted.id == this.tekateki[id][0].id) {
-    const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
-    if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
-      global.db.data.users[m.sender].estrellas += this.tekateki[id][2];
-      m.reply(`🤍 *Respuesta correcta!*\n+${this.tekateki[id][2]} Centavos`);
-      clearTimeout(this.tekateki[id][3]);
-      delete this.tekateki[id];
-    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras!`);
-    else m.reply('Respuesta incorrecta!');
-  }
-  return !0;
-};
-handler.exp = 0;
-export default handler;
+import similarity from 'similarity'
+const threshold = 0.72
+
+var handler = m => m
+handler.before = async function (m) {
+
+let id = m.chat
+if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0
+this.tekateki = this.tekateki ? this.tekateki : {}
+if (!(id in this.tekateki)) return m.reply('*⚠️ ESE ACERTIJO YA HA TERMINADO!*')
+if (m.quoted.id == this.tekateki[id][0].id) {
+let json = JSON.parse(JSON.stringify(this.tekateki[id][1]))
+if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
+global.db.data.users[m.sender].exp += this.tekateki[id][2]
+m.reply(`*✅ RESPUESTA CORRECTA!*\n+${this.tekateki[id][2]} Exp`)
+m.react(done)
+clearTimeout(this.tekateki[id][3])
+delete this.tekateki[id]
+} else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`*⚠️ CASI LO LOGRAS!*`)
+else m.reply('*⚠️ RESPUESTA INCORRECTA!*')}
+m.react(error)
+return !0
+
+}
+handler.exp = 0
+
+export default handler
