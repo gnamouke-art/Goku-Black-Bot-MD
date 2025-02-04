86df5921faafbb2e1cbd9553fa5b04d53209e464
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-04 14:04:49 -0300

    Actualizar Juegos+18.js

--- plugins/Juegos+18.js
+++ plugins/Juegos+18.js
@@ -1,1 +1,67 @@
-//☆☆☆☆☆
+//Agradecimiento a José elver por terminar el code
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    const githubRepoURL = 'https://github.com/JawadYTX/KHAN-MD';
+
+    try {
+        const [, username, repoName] = githubRepoURL.match(/github\.com\/([^/]+)\/([^/]+)/);
+        const response = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+
+        if (!response.ok) {
+            throw new Error(`La solicitud a la API de GitHub falló con el estado ${response.status}`);
+        }
+
+        const repoData = await response.json();
+        let CodesJose = '`📂  I N F O R M A C I Ó N  D E  R E P O S I T O R I O`\n\n';
+        CodesJose += `    ✩  *NOMBRE DEL BOT* : ${repoData.name}\n`;
+        CodesJose += `    ✩  *NOMBRE DEL PROPIETARIO* : ${repoData.owner.login}\n`;
+        CodesJose += `    ✩  *ESTRELLAS* : ${repoData.stargazers_count}\n`;
+        CodesJose += `    ✩  *FORKS* : ${repoData.forks_count}\n`;
+        CodesJose += `    ✩  *ENLACE DE GITHUB* : ${repoData.html_url}\n`;
+        CodesJose += `    ✩  *DESCRIPCIÓN* : ${repoData.description || 'Sin descripción disponible'}\n\n`;
+        CodesJose += `*¡No olvides darle una estrella y hacer fork al repositorio!*\n\n`;
+        CodesJose += `> *Impulsado por Jose Elber*`;
+
+        await conn.sendMessage(m.chat, {
+            image: { url: 'https://files.catbox.moe/juroe8.jpg' },
+            caption: CodesJose,
+            contextInfo: { 
+                mentionedJid: [m.sender],
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363354023106228@newsletter',
+                    newsletterName: 'JawadTechX',
+                    serverMessageId: 143
+                }
+            }
+        }, { quoted: m });
+
+        await conn.sendMessage(m.chat, {
+            audio: { url: 'https://github.com/JawadYTX/KHAN-DATA/raw/refs/heads/main/autovoice/repo.m4a' },
+            mimetype: 'audio/mp4',
+            ptt: true,
+            contextInfo: { 
+                mentionedJid: [m.sender],
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363354023106228@newsletter',
+                    newsletterName: 'JawadTechX',
+                    serverMessageId: 143
+                }
+            }
+        }, { quoted: m });
+
+    } catch (error) {
+        await conn.reply(m.chat, "Lo siento, ocurrió un error al obtener la información del repositorio. Por favor, intenta de nuevo más tarde.", m);
+    }
+}
+
+handler.tags = ['info'];
+handler.help = ['repo', 'repo', 'b', 'b'];
+handler.command = ['repo', 'repo', 'repo', 'repo'];
+handler.register = true;
+
+export default handler;
