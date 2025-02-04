7f065c7e15841ddc4094973e92c003fa435d5cce
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-04 14:26:58 -0300

    Goku-Black-Bot-MD

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -7,7 +7,7 @@ let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.js
 m.reply(`
 *≡ SCRIPT*
 
-▢ Git : ${_package.homepage}
+▢ Git : https://github.com/Ivanmods15/Goku-Black-Bot-MD
 `.trim())
     
 }
