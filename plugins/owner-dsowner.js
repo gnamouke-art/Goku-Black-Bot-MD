d666c3a7e4e7b594e1ff05e08f292d4537b5d4d5
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-05 21:53:25 -0300

    Actualizar owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -6,17 +6,17 @@ import path from 'path'
 var handler = async (m, { conn, usedPrefix }) => {
 
 if (global.conn.user.jid !== conn.user.jid) {
-return conn.reply(m.chat, '🚩 *Utiliza este comando directamente en el número principal del Bot*', m, rcanal, )
+return conn.reply(m.chat, '☯︎ *Utiliza este comando directamente en el número principal del Bot*', m, rcanal, )
 }
-await conn.reply(m.chat, '🚩 *Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...*', m, rcanal, )
+await conn.reply(m.chat, '🂱 *Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...*', m, rcanal, )
 m.react(rwait)
 
-let sessionPath = './MiniSession/'
+let sessionPath = './Blacksession/'
 
 try {
 
 if (!existsSync(sessionPath)) {
-return await conn.reply(m.chat, '🚩 *La carpeta está vacía*', m, rcanal, )
+return await conn.reply(m.chat, '✈︎ *La carpeta está vacía*', m, rcanal, )
 }
 let files = await fs.readdir(sessionPath)
 let filesDeleted = 0
@@ -27,16 +27,16 @@ filesDeleted++;
 }
 }
 if (filesDeleted === 0) {
-await conn.reply(m.chat, '🚩 *La carpeta esta vacía*',  m, rcanal, )
+await conn.reply(m.chat, '✈︎ *La carpeta esta vacía*',  m, rcanal, )
 } else {
 m.react(done)
-await conn.reply(m.chat, `🚩 *Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json*`,  m, rcanal, )
-conn.reply(m.chat, `🚩 *¡Hola! ¿logras verme?*`, m, rcanal, )
+await conn.reply(m.chat, `⚠︎ *Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json*`,  m, rcanal, )
+conn.reply(m.chat, `𒊹︎︎︎ *¿Me ves o no futuro cliente?*`, m, rcanal, )
 
 }
 } catch (err) {
 console.error('Error al leer la carpeta o los archivos de sesión:', err);
-await conn.reply(m.chat, '🚩 *Ocurrió un fallo*',  m, rcanal, )
+await conn.reply(m.chat, '𖠌 *Ocurrió un fallo*',  m, rcanal, )
 }
 
 }
