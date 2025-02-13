d4e517bfdb6fb6b4f90831aa2999a03fe124ddab
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-13 18:02:38 -0300

    Actualizar vsvs.js

--- plugins/vsvs.js
+++ plugins/vsvs.js
@@ -1,1 +1,54 @@
 //𝘗𝘰𝘸𝘦𝘳 𝘣𝘺 𝘪𝘷𝘢𝘯
+let handler = async (m, { conn }) => {
+    const mascotas = [
+        'Perro',
+        'Gato',
+        'Loro',
+        'Tortuga',
+        'Conejo',
+        'Hámster',
+        'Pez',
+        'Iguana',
+        'Cobaya',
+        'Serpiente',
+        'Gallo',
+        'Erizo',
+        'Pájaro',
+        'Rana',
+        'Chinchilla',
+        'Foca',
+        'Gato esfinge',
+        'Dragón de Komodo',
+        'Canguro',
+        'Koala',
+        'Axolotl',
+        'Capibara',
+        'Mapache',
+        'Zorro',
+        'Hurón',
+        'Cacatúa',
+        'Gecko',
+        'Camaleón',
+        'Caracol',
+        'Cuyo',
+        'Jerbo',
+        'Ninfa',
+        'Canario',
+        'Pony',
+        'Alpaca',
+        'Cerdo vietnamita',
+    ];
+
+    const getRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+    const mascotaAleatoria = getRandom(mascotas);
+
+    conn.reply(m.chat, `¡Tu mascota aleatoria es: ${mascotaAleatoria}!`, m);
+}
+
+handler.help = ['mascotaAleatoria'];
+handler.tags = ['juegos'];
+handler.command = ['mascotaAleatoria', 'mascota'];
+handler.register = true;
+
+export default handler;
