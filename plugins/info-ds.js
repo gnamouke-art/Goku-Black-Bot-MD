09f129d028c1e5436b91240c3a4b10d7103a54a0
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-14 22:44:20 -0300

    Actualizar info-ds.js

--- plugins/info-ds.js
+++ plugins/info-ds.js
@@ -10,7 +10,7 @@ return conn.reply(m.chat, '🚩 *Utiliza este comando directamente en el número
 }
 
 let chatId = m.isGroup ? [m.chat, m.sender] : [m.sender]
-let sessionPath = './BlackSesion/'
+let sessionPath = './Blacksesion/'
 
 try {
 
