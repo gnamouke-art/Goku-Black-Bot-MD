2dea1ca71e60a0cac7f89f9a50c14e0bf8d373e1
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-16 10:38:40 -0300

    Actualizar _validComd.js

--- plugins/_validComd.js
+++ plugins/_validComd.js
@@ -1,19 +1,67 @@
-//:)
-import speed from 'performance-now'
-import { spawn, exec, execSync } from 'child_process'
+/*codigo creado por Jose Mods 
+dejen creditos no sean ratas y traído por Rayo-ofc*/
+import axios from 'axios';
 
-let handler = async (m, { conn }) => {
-         let timestamp = speed();
-         let latensi = speed() - timestamp;
-         exec(`neofetch --stdout`, (error, stdout, stderr) => {
-          let child = stdout.toString("utf-8");
-          let ssd = child.replace(/Memory:/, "Ram:");
+const ddownr = {
+  download: async (query) => {
+    const apiUrl = `https://carisys.online/api/downloads/youtube/play?query=${encodeURIComponent(query)}`;
 
-          m.reply(`${saludo}\n\n*El Bot Tiene 2 Metodos Para Vincularse A Tu WhatsApp.*\n\nSon:\n\n- Texto De 8 Digitos.\n- Codigo Qr.\n\n*Espero Y Instales El Bot Y Uses El Metodo Que Puedas.*`);
-            });
+    try {
+      const response = await axios.get(apiUrl);
+
+      if (response.data.status) {
+        const { resultado } = response.data;
+        return {
+          title: resultado.titulo,
+          duration: resultado.tempo,
+          views: resultado.views,
+          audio: resultado.audio,
+          thumbnail: resultado.imagem,
+        };
+      } else {
+        throw new Error('La API devolvió un estado no exitoso.');
+      }
+    } catch (error) {
+      console.error('Error al descargar:', error);
+      throw error;
+    }
+  }
+};
+
+const handler = async (m, { conn, text }) => {
+  try {
+    if (!text.trim()) {
+      return conn.reply(m.chat, `🌹 Ingresa el nombre de la música a descargar.`, m);
+    }
+
+    const videoInfo = await ddownr.download(text);
+
+    const infoMessage = '`乂  Y O U T U B E  -  D O W N L O A D`\n\n' +
+                        `    ✩   *Título* : ${videoInfo.title}\n` +
+                        `    ✩   *Duración* : ${videoInfo.duration}\n` +
+                        `    ✩   *Vistas* : ${formatViews(videoInfo.views)}\n` +
+                        '> *- ↻ Enviando su audio como documento. Espere un momento...*';
+
+    const thumb = (await conn.getFile(videoInfo.thumbnail))?.data;
+
+    await conn.sendFile(m.chat, thumb, 'thumbnail.jpg', infoMessage, m);
+
+    await conn.sendMessage(m.chat, { 
+      document: { url: videoInfo.audio }, 
+      mimetype: "audio/mpeg", 
+      fileName: `${videoInfo.title}.mp3`
+    }, { quoted: m });
+
+  } catch (error) {
+    return m.reply(`⚠️ *Error:* ${error.message}`);
+  }
+};
+
+handler.command = handler.help = ['ytmp3doc', '2ytmp3'];
+handler.tags = ['downloader'];
+
+export default handler;
+
+function formatViews(views) {
+  return views.toLocaleString(); 
 }
-handler.help = ['info']
-handler.tags = ['main']
-handler.command = ['we']
-handler.register = true
-export default handler
