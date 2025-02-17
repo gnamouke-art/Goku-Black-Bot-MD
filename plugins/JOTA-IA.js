aa6799c3465a58fd541d1f7c9a999e1777d0455b
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-17 11:24:47 -0300

    Goku-Black-Bot-MD

--- plugins/JOTA-IA.js
+++ plugins/JOTA-IA.js
@@ -1,18 +1,31 @@
-import axios from 'axios'
+import path from 'path';
+import uploadImage from '../lib/uploadImage.js'
+import { sticker } from '../lib/sticker.js';
 
-let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, `ingresa un texto para hablar con Jota`, m)
-  
-try {
-let api = `https://api.siputzx.my.id/api/m/brat?text=${text}`
-let json = await axios.get(api, { responseType: 'arraybuffer'})
-let img = Buffer.from(json.data, 'binary')
-await conn.sendMessage(m.chat, { sticker: img }, { quoted: m })
+let handler = async (m, { conn, usedPrefix }) => {
+    let who;
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
+    else who = m.chat;
+    let user = global.db.data.users[who];
+    let name = conn.getName(who);
+    let name2 = conn.getName(m.sender);
+   // m.react('⏳');
+    await conn.sendMessage(m.chat, { react: { text: '🥵', key: m.key } })
+    let str = `${name2} explotaste el culo a ${name}`.trim();
+    if (m.isGroup){
 
-} catch (error) {
-console.error(error)    
-}}
+    let pp = 'https://telegra.ph/file/21543bac2383ce0fc6f51.mp4'
+    let pp2 = 'https://telegra.ph/file/89891693613651230d6f0.mp4'
+    const videos = [pp1, pp2];
+    const video = videos[Math.floor(Math.random() * videos.length)];
+    conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption:str, mentions: [m.sender] },{ quoted: estilo })
+    };
 
-handler.command = ['jota']
+  //  m.react('🔥');
+}
 
-export default handler
+handler.help = ['culodiomar @tag'];
+handler.tags = ['fun'];
+handler.command = ['explotarculodiomar','darduroadiomar']
+
+export default handler;
