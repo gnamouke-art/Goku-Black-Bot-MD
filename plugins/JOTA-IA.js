d00bf59c1683b6f048a2faf746df20e15b1e1311
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-17 11:56:44 -0300

    Actualizar JOTA

--- plugins/JOTA-IA.js
+++ plugins/JOTA-IA.js
@@ -6,7 +6,7 @@ let img = await (await fetch('https://files.catbox.moe/mlvo06.mp4')).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 let txt = `le explotaste el culo a diomar que gay que eres we`
-await conn.sendFile(m.chat, m, null, rcanal)
+await conn.sendFile(m.chat, m, img, "Thumbnail.jpg", null, rcanal)
 }
 handler.help = ['explotarculodiomar']
 handler.tags = ['explotarculodiomar']
