6ef7fca87b35529dbcc02cbc442f1a16e004f5e0
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-17 11:46:23 -0300

    XD

--- plugins/JOTA-IA.js
+++ plugins/JOTA-IA.js
@@ -1,1 +1,14 @@
-//XD
+//Codigo creado por Ivan 
+import fetch from 'node-fetch'
+
+let handler  = async (m, { conn, usedPrefix, command }) => {
+let img = await (await fetch('https://files.catbox.moe/mlvo06.mp4')).buffer()
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+let txt = `${name2} le explotaste el culo a diomar que gay que eres we`
+await conn.sendFile(m.chat, m, null, rcanal)
+}
+handler.help = ['explotarculodiomar']
+handler.tags = ['explotarculodiomar']
+handler.command = /^(follardiomar)$/i
+export default handler
