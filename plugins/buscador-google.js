1919e374fce5eef941b62d2287ea88da941312c6
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-17 00:40:01 -0300

    Goku-Black-Bot-MD

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -1,28 +1,43 @@
-import {googleIt} from '@bochilteam/scraper'
-import google from 'google-it'
-import axios from 'axios'
+import {googleIt} from '@bochilteam/scraper';
+import google from 'google-it';
+import axios from 'axios';
 let handler = async (m, { conn, command, args, usedPrefix }) => {
 const fetch = (await import('node-fetch')).default;
-const text = args.join` `
-if (!text) return conn.reply(m.chat, '🍟 Ingresa lo que deseas buscar en Google.', m, rcanal)
-conn.reply(m.chat, `🚩 Buscando Su Información...`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
-const url = 'https://google.com/search?q=' + encodeURIComponent(text)
+const text = args.join` `;
+if (!text) throw `⚠️ 𝙌𝙪𝙚 𝙚𝙨𝙩𝙖 𝙗𝙪𝙨𝙘𝙖𝙣𝙙𝙤 🤔 𝙀𝙨𝙘𝙧𝙞𝙗𝙖 𝙡𝙤 𝙦𝙪𝙚 𝙦𝙪𝙞𝙚𝙧𝙖 𝙗𝙪𝙨𝙘𝙖𝙧\n• 𝙀𝙟: ${usedPrefix + command} loli`
+m.react("⌛") 
+try {
+const res = await fetch(`${apis}/search/googlesearch?query=${encodeURIComponent(text)}`);
+const data = await res.json();
+
+if (data.status && data.data && data.data.length > 0) {
+let teks = `\`🔍 𝘙𝘌𝘚𝘜𝘓𝘛𝘈𝘋𝘖𝘚 𝘋𝘌:\` ${text}\n\n`;
+for (let result of data.data) {
+teks += `*${result.title}*\n_${result.url}_\n_${result.description}_\n\n─────────────────\n\n`;
+}
+
+const ss = `https://image.thum.io/get/fullpage/https://google.com/search?q=${encodeURIComponent(text)}`;
+conn.sendFile(m.chat, ss, 'result.png', teks, fkontak, false, fake);
+m.react("✅")                 
+}} catch (error) {
+try {
+const url = 'https://google.com/search?q=' + encodeURIComponent(text);
 google({'query': text}).then(res => {
-let teks = `🍟 *Resultado de* : ${text}\n\n`
+let teks = `\`🔍 𝘙𝘌𝘚𝘜𝘓𝘛𝘈𝘋𝘖𝘚 𝘋𝘌:\` ${text}\n\n*${url}*\n\n`
 for (let g of res) {
-teks += `🐢 *Titulo ∙* ${g.title}\n🚩 *Info ∙* ${g.snippet}\n🔗 *Url ∙* ${g.link}\n\n`
+teks += `_${g.title}_\n_${g.link}_\n_${g.snippet}_\n\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n\n`
 }
-conn.reply(m.chat, teks, m, rcanal)
-})
-}
-handler.help = ['google <búsqueda>']
-handler.tags = ['buscador']
-handler.command = ['google']
-handler.group = true;
-handler.register = true
+const ss = `https://image.thum.io/get/fullpage/${url}`
+conn.sendFile(m.chat, ss, 'error.png', teks, fkontak, false, fake)
+});
+m.react("✅")       
+} catch (e) {
+handler.limit = 0;
+console.log(e);
+m.react("❌")  
+}}}
+handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
+handler.tags = ['buscadores']
+handler.command = /^googlef?$/i
+handler.register = true   
 export default handler
