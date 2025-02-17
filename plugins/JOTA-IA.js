ce0f19217b3349b11b0fc126310870f19f6a6a5b
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-17 11:32:14 -0300

    Actualizar JOTA-IA.js

--- plugins/JOTA-IA.js
+++ plugins/JOTA-IA.js
@@ -5,7 +5,6 @@ import { sticker } from '../lib/sticker.js';
 // m.react('⏳');
     await conn.sendMessage(m.chat, { react: { text: '🥵', key: m.key } })
     let str = `Le explotaste el culo a diomar we`.trim();
-    if (m.isGroup){
 
     let pp1 = 'https://telegra.ph/file/21543bac2383ce0fc6f51.mp4'
     let pp2 = 'https://telegra.ph/file/89891693613651230d6f0.mp4'
