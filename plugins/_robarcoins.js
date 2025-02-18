2e52119883f8357e17a21e11e43b6a7bfe7313bc
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-02-18 18:15:32 -0300

    Actualizar _robarcoins.js

--- plugins/_robarcoins.js
+++ plugins/_robarcoins.js
@@ -1,25 +1,41 @@
+import db from '../lib/database.js';
 import MessageType from '@whiskeysockets/baileys';
-const pajak = 0;
-const handler = async (m, {conn, text}) => {
-  let who;
-  if (m.isGroup) who = m.mentionedJid[0];
-  else who = m.chat;
-  if (!who) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰 𝙰 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙲𝙾𝙽 𝙴𝙻 @𝚝𝚊𝚐*';
-  const txt = text.replace('@' + who.split`@`[0], '').trim();
-  if (!txt) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙻𝙰 𝙲𝙰𝙽𝚃𝙸𝙳𝙰𝙳 𝙳𝙴 𝙴𝚇𝙿𝙴𝚁𝙸𝙴𝙽𝙲𝙸𝙰 (𝚇𝙿) 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙰 𝙰𝙽̃𝙰𝙳𝙸𝚁*';
-  if (isNaN(txt)) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚂𝙸𝙼𝙱𝙾𝙻𝙾 𝙽𝙾 𝙰𝙳𝙼𝙸𝚃𝙸𝙳𝙾, 𝚂𝙾𝙻𝙾 𝙽𝚄𝙼𝙴𝚁𝙾𝚂!*';
-  const xp = parseInt(txt);
-  let exp = xp;
-  const pjk = Math.ceil(xp * pajak);
-  exp += pjk;
-  if (exp < 1) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙼𝙸𝙽𝙸𝙼𝙾 𝙳𝙴 𝙴𝚇𝙿𝙴𝚁𝙸𝙴𝙽𝙲𝙸𝙰 (𝚇𝙿) 𝙿𝙰𝚁𝙰 𝙰𝙽̃𝙰𝙳𝙸𝚁 𝙴𝚂 𝟷*';
-  const users = global.db.data.users;
-  users[who].exp += xp;
-  m.reply(`≡ *𝚇𝙿 𝙰𝙽̃𝙰𝙳𝙸𝙳𝙾*
-┌──────────────
-▢  *𝚃𝚘𝚝𝚊𝚕:* ${xp}
-└──────────────`);
+
+let pajak = 0;
+
+const handler = async (m, { conn, text }) => {
+    let who;
+    if (m.isGroup) {
+        if (m.mentionedJid.length > 0) {
+            who = m.mentionedJid[0];
+        } else {
+            const quoted = m.quoted ? m.quoted.sender : null;
+            who = quoted ? quoted : m.chat;
+        }
+    } else {
+        who = m.chat;
+    }
+    
+    if (!who) return m.reply('*🍬 Por favor, menciona al usuario o cita un mensaje.*');
+
+    const txt = text.replace('@' + who.split`@`[0], '').trim();
+    if (!txt) return m.reply('*🍬 Ingresa la cantidad de experiencia (XP) que deseas añadir.*');
+    if (isNaN(txt)) return m.reply('🍭 *Solo números son permitidos.*');
+    
+    const xp = parseInt(txt);
+    let exp = xp;
+    const pjk = Math.ceil(xp * pajak);
+    exp += pjk;
+    
+    if (exp < 1) return m.reply('🍬 El mínimo de experiencia (XP) para añadir es *1*.');
+    
+    const users = global.db.data.users;
+    users[who].exp += xp;
+    
+    m.reply(`✨ XP Añadido: *${xp}* \n@${who.split('@')[0]}, recibiste ${xp} XP`, null, { mentions: [who] });
 };
+
 handler.command = ['añadirxp', 'addexp'];
 handler.rowner = true;
+
 export default handler;
