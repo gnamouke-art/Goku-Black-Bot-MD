2d405a622b5867837b1fc25b71e8949548f8be71
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-03-06 17:41:45 -0300

    Actualizar fun-penetrar.js

--- plugins/fun-penetrar.js
+++ plugins/fun-penetrar.js
@@ -1,5 +1,4 @@
 let handler = async (m, { conn, command, text }) => {
-if (!db.data.chats[m.chat].nsfwhot && m.isGroup) throw conn.reply(m.chat
 if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*`
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 conn.reply(m.chat, `
