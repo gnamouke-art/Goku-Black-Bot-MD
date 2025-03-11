d3f3e927a0324803cafdd30a233d3f2556900097
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-03-11 20:32:01 -0300

    Actualizar descargas-mediafire.js

--- plugins/descargas-mediafire.js
+++ plugins/descargas-mediafire.js
@@ -1,17 +1,19 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw m.reply(`Ingresa un link de mediafire\n*👻 Ejemplo:* ${usedPrefix}${command} https://www.mediafire.com/file/2v2x1p0x58qomva/WhatsApp_Messenger_2.24.21.8_beta_By_WhatsApp_LLC.apk/file`);
+
+if (!text) throw m.reply(`${emoji} Por favor, ingresa un link de mediafire.`);
 conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
         let ouh = await fetch(`https://api.agatz.xyz/api/mediafire?url=${text}`)
-  let gyh = await ouh.json()
-        await conn.sendFile(m.chat, gyh.data[0].link, `${gyh.data[0].nama}`, `*📝 Nombre:* ${gyh.data[0].nama}\n*🎮 Tamaño:* ${gyh.data[0].size}\n*💾 Extensión:* ${gyh.data[0].mime}\n> ${dev}`, m)
+  let gyh = await ouh.json() 
+        await conn.sendFile(m.chat, gyh.data[0].link, `${gyh.data[0].nama}`, `乂  *¡MEDIAFIRE - DESCARGAS!*  乂\n\n✩ *Nombre* : ${gyh.data[0].nama}\n✩ *Peso* : ${gyh.data[0].size}\n✩ *MimeType* : ${gyh.data[0].mime}\n> ${dev}`, m)       
         await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
 }
 handler.help = ['mediafire']
 handler.tags = ['descargas']
-handler.command = /^(mediafire|mf)$/i
-handler.premium = false
-handler.estrellas = 5
+handler.command = ['mf', 'mediafire']
+handler.estrellas = 20
 handler.register = true
+handler.group = true
+
 export default handler
