4e4035173dbc092d14001bfa9e1ba0267476a241
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-03-19 17:27:10 -0300

    Actualizar Vamos-Albirroja.js

--- plugins/Vamos-Albirroja.js
+++ plugins/Vamos-Albirroja.js
@@ -1,6 +1,6 @@
 //Codigo creado por Ivan 
 import fetch from 'node-fetch'
-
+await m.react('🇵🇾');
 let handler  = async (m, { conn, usedPrefix, command }) => {
 let img = await (await fetch(`https://files.catbox.moe/8piv8y.mp4`)).buffer()
 const more = String.fromCharCode(8206)
@@ -8,7 +8,6 @@ const readMore = more.repeat(4001)
 let txt = `*EMPIEZA LA NUEVA MISION VAMOS ALBIRROJA QUERIDA 🇵🇾*`
 await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, rcanal)
 }
-  await m.react('🇵🇾');
 handler.help = ['albirroja']
 handler.tags = ['albirroja']
 handler.command = ['albirroja', '🇵🇾']
