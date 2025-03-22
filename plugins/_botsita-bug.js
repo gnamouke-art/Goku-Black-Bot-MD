c3df431b5b2ce24c0b496badfdb5800656a24562
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-03-22 15:24:41 -0300

    Actualizar _botsita-bug.js

--- plugins/_botsita-bug.js
+++ plugins/_botsita-bug.js
@@ -1,11 +1,1 @@
-import fetch from 'node-fetch'
-
-let HS = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, '`ingresa un texto para hablar con Cami-ia`', m)
-} catch (error) {
-console.error(error)
-}}
-
-HS.command = ['verreg']
-
-export default HS
+//bug
