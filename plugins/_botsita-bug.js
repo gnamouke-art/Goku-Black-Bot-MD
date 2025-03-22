47867db93eac061b5ae1e54e206256f21ecf68b4
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-03-22 15:29:27 -0300

    Goku-Black-Bot-MD

--- plugins/_botsita-bug.js
+++ plugins/_botsita-bug.js
@@ -4,4 +4,5 @@ const handler = async (m, {conn}) => {
 handler.command = ['verreg']
 export default handler;
 
-global.verreg = `> Mira aquí tu reg 👇`;
+global.verreg = ``Mira aquí tu reg 👇`
+https://whatsapp.com/channel/0029VasrQq2Gk1G1THOKwS2L`;
