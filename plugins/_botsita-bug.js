c4d39a33e9c67d6e7dec3ee93e210cf9ba25384a
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-03-22 15:23:57 -0300

    Goku-Black-Bot-MD

--- plugins/_botsita-bug.js
+++ plugins/_botsita-bug.js
@@ -2,12 +2,6 @@ import fetch from 'node-fetch'
 
 let HS = async (m, { conn, command, text, usedPrefix }) => {
 if (!text) return conn.reply(m.chat, '`ingresa un texto para hablar con Cami-ia`', m)
-try {
-let json = await api.json()
-let JT = { contextInfo: { externalAdReply: { title: 'VER - REG', body: null,  mediaType: 1, previewType: 0, mediaUrl: sourceUrl: thumbnailUrl: renderLargerThumbnail: false }}}
-
-  await conn.reply(m.chat, json.result, m, JT)
-
 } catch (error) {
 console.error(error)
 }}
