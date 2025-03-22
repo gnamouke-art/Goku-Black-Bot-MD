bf1f43e954def646d32753318572e9db39f510d3
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-03-22 15:22:54 -0300

    Goku-Black-Bot-MD

--- plugins/_botsita-bug.js
+++ plugins/_botsita-bug.js
@@ -3,9 +3,8 @@ import fetch from 'node-fetch'
 let HS = async (m, { conn, command, text, usedPrefix }) => {
 if (!text) return conn.reply(m.chat, '`ingresa un texto para hablar con Cami-ia`', m)
 try {
-let api = await fetch(`https://api.davidcyriltech.my.id/ai/chatbot?query=${text}`)
 let json = await api.json()
-let JT = { contextInfo: { externalAdReply: { title: 'Cami - IA', body: null,  mediaType: 1, previewType: 0, mediaUrl: 'https://Cami-IA.com', sourceUrl: 'https://Cami-IA.com', thumbnailUrl: 'https://qu.ax/OfoAC.jpg', renderLargerThumbnail: false }}}
+let JT = { contextInfo: { externalAdReply: { title: 'VER - REG', body: null,  mediaType: 1, previewType: 0, mediaUrl: sourceUrl: thumbnailUrl: renderLargerThumbnail: false }}}
 
   await conn.reply(m.chat, json.result, m, JT)
 
@@ -13,6 +12,6 @@ let JT = { contextInfo: { externalAdReply: { title: 'Cami - IA', body: null,  me
 console.error(error)
 }}
 
-HS.command = ['iacami']
+HS.command = ['verreg']
 
 export default HS
