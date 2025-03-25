361f1fcd29f7f9b371b8a34071e067486690ce9a
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-03-25 11:42:14 -0400

    Quitando syntax-error

--- plugins/Vamos-Albirroja.js
+++ plugins/Vamos-Albirroja.js
@@ -1,14 +1,1 @@
-//Codigo creado por Ivan 
-import fetch from 'node-fetch'
-await m.react('🇵🇾');
-let handler  = async (m, { conn, usedPrefix, command }) => {
-let img = await (await fetch(`https://files.catbox.moe/8piv8y.mp4`)).buffer()
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-let txt = `*EMPIEZA LA NUEVA MISION VAMOS ALBIRROJA QUERIDA 🇵🇾*`
-await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, null, rcanal)
-}
-handler.help = ['albirroja']
-handler.tags = ['albirroja']
-handler.command = ['albirroja', '🇵🇾']
-export default handler
+//IVAN 
